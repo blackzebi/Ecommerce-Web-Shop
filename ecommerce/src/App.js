c153ee33208b2d0ee7267a4d2702bef00533087c
blackzebi <caominhtri18@gmail.com>
2022-03-13 16:53:53 +0700

    updated function checkout

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { Products, Navbar } from "./components";
+import { Products, Navbar, Cart, Checkout } from "./components";
 import { commerce } from "./lib/commerce";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   const [products, setProducts] = useState([]);
   const [cart, setCart] = useState({});
+
   const fetchProducts = async () => {
     const { data } = await commerce.products.list();
     setProducts(data);
@@ -15,9 +17,34 @@ function App() {
   };
 
   const handeleAddToCart = async (productId, quanitity) => {
-    const item = await commerce.cart.add(productId, quanitity);
+    // line 20-23 === line 24-26. Giá trị cách code giống nhau nhưng sử dụng cấu trúc destructuring để để tiết kiết và gọn hơn so với bình thường
+    // const item = await commerce.cart.add(productId, quanitity);
 
-    setCart(item.cart);
+    // setCart(item.cart);
+    const { cart } = await commerce.cart.add(productId, quanitity);
+
+    setCart(cart);
+  };
+
+  const handleUpdateCartQty = async (productId, quantity) => {
+    // const response = await commerce.cart.update(productId, {quantity})
+
+    // setCart(response.cart)
+    const { cart } = await commerce.cart.update(productId, { quantity });
+
+    setCart(cart);
+  };
+
+  const handleRemoveFromCart = async (productId) => {
+    const { cart } = await commerce.cart.remove(productId);
+
+    setCart(cart);
+  };
+
+  const handleEmptyCart = async () => {
+    const { cart } = await commerce.cart.empty();
+
+    setCart(cart);
   };
 
   useEffect(() => {
@@ -26,10 +53,27 @@ function App() {
   }, []);
 
   return (
-    <div>
-      <Navbar />
-      <Products products={products} onAddToCart={handeleAddToCart} />
-    </div>
+    <Router>
+      <div>
+        <Navbar totalItems={cart.total_items} />
+        <Switch>
+          <Route exact path="/">
+            <Products products={products} onAddToCart={handeleAddToCart} />
+          </Route>
+          <Route exact path="/cart">
+            <Cart
+              cart={cart}
+              handleUpdateCartQty={handleUpdateCartQty}
+              handleEmptyCart={handleEmptyCart}
+              handleRemoveFromCart={handleRemoveFromCart}
+            />
+          </Route>
+          <Route exact path="/checkout">
+            <Checkout />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
